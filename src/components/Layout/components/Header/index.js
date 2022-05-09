4ce982c7e729ed3d5dc68641017273bb5cc57ae5
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-05-09 18:15:08 +0700

    header 10%

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
 import classNames from 'classnames/bind'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faCircleXmark,
+	faMagnifyingGlass,
+	faSpinner,
+} from '@fortawesome/free-solid-svg-icons'
+
 import styles from './Header.module.scss'
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
@@ -8,7 +16,27 @@ function Header() {
 		<header className={cx('wrapper')}>
 			<div className={cx('inner')}>
 				{/* Logo */}
+				<div className={cx('logo')}>
+					<img src={images.logo} alt='tiktok' />
+				</div>
 				{/* Search */}
+				<div className={cx('search')}>
+					<input
+						type='text'
+						placeholder='Search'
+						spellCheck={false}
+					/>
+					<button className={cx('clear')}>
+						<FontAwesomeIcon icon={faCircleXmark} />
+					</button>
+					<FontAwesomeIcon
+						className={cx('loading')}
+						icon={faSpinner}
+					/>
+					<button className={cx('search-btn')}>
+						<FontAwesomeIcon icon={faMagnifyingGlass} />
+					</button>
+				</div>
 			</div>
 		</header>
 	)
